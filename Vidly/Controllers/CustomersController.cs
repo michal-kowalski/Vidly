fa260e6fb9025fd54d9866a0226d6d96416a1e77
michal-kowalski <kowalski@tfs.pl>
2017-06-12 13:04:45 +0200

    DB

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -10,17 +10,28 @@ namespace Vidly.Controllers
 {
 	public class CustomersController : Controller
 	{
-		// GET: Customers
+		private ApplicationDbContext _context;
+		public CustomersController()
+		{
+			_context = new ApplicationDbContext();
+		}
+		protected override void Dispose(bool disposing)
+		{
+			_context.Dispose();
+		}
+										   // GET: Customers
 		public ActionResult Index()
 		{
 			var movie = new Movie() { Name = "Shrek!", Id = 1 };
-			var customers = new List<Customer>
+			var customers = _context.Customers.ToList();
+
+			/*var customers = new List<Customer>
 			{
 				new Customer {Name = "John Smith", Id = 0 },
 				new Customer {Name = "Jon Doe", Id = 1 },
 				new Customer {Name = "Jane Doe", Id = 2 },
 				new Customer {Name = "Customer 4", Id = 3 }
-			};
+			};*/
 
 			var viewModel = new RandomMovieViewModel
 			{
@@ -33,21 +44,19 @@ namespace Vidly.Controllers
 
 		public ActionResult CustomersDetails(int id)
 		{
-			var customers = new List<Customer>
+			var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+			if (customer == null)
+				return HttpNotFound();
+
+			var customers = new Customer();
+
+			var ViewModel = new RandomMovieViewModel
 			{
-				new Customer {Name = "John Smith", Id = 0, Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." },
-				new Customer {Name = "Jon Doe", Id = 1, Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." },
-				new Customer {Name = "Jane Doe", Id = 2, Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." },
-				new Customer {Name = "Customer 4", Id = 3, Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." },
-				new Customer {Name = "Customer 5", Id = 4, Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." }
+				Customer = customer
 			};
 
-			var viewModel = new RandomMovieViewModel
-			{
-				Customer = customers[id]
-			};
-
-			return View(viewModel);
+			
+			return View(ViewModel);
 		}
 
 	}
