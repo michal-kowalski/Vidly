3b7f45a3127f01d79b28ff02195a2793631c19b8
michal-kowalski <kowalski@tfs.pl>
2017-06-19 12:23:17 +0200

    dropdown list

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -60,7 +60,12 @@ namespace Vidly.Controllers
 
         public ActionResult New()
         {
-            return View();
+			var membershipTypes = _context.MembershipTypes.ToList();
+			var viewModel = new NewCustomerViewModel
+			{
+				MembershipTypes = membershipTypes
+			};
+            return View(viewModel);
         }
 
 	}
